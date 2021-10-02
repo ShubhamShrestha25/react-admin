e13bfd0ac521bb07d789338e907064c4cf97b4d1
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-10-02 15:06:13 +0545

    Charts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
+import { userdata } from "../../DummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userdata}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
