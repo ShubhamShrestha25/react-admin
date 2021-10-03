e99a2a10987934b3c6f1396101139c36a3367766
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-10-03 10:21:39 +0545

    material-ui table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userlist/UserList";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/DummyData.js
+++ src/DummyData.js
@@ -48,3 +48,69 @@ export const userdata = [
     "Active User": 4999,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Jack Snow",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "jack@hotmail.com",
+    status: "active",
+    transaction: "$200",
+  },
+  {
+    id: 2,
+    username: "Rich Rider",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "RRider@hotmail.com",
+    status: "active",
+    transaction: "$1200",
+  },
+  {
+    id: 3,
+    username: "Rohit Kumar",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "Rohit@gmail.com",
+    status: "active",
+    transaction: "$500",
+  },
+  {
+    id: 4,
+    username: "Shubham Shrestha",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "ShubhamShrestha@gmail.com",
+    status: "active",
+    transaction: "$755",
+  },
+  {
+    id: 5,
+    username: "Jonny Rich",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "JonnyBoi@gmail.com",
+    status: "active",
+    transaction: "$300",
+  },
+  {
+    id: 6,
+    username: "RawzuStha",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "SthaRawzu@hotmail.com",
+    status: "active",
+    transaction: "$1136",
+  },
+  {
+    id: 7,
+    username: "Sanju Sam",
+    avatar:
+      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    email: "Sammy@yahoo.com",
+    status: "active",
+    transaction: "$651",
+  },
+];
